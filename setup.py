f27a1dca8ec376b2c7ee52d51b4094c7dbb4e6dc
Zhangyi <loeyae@gmail.com>
2023-03-26 12:55:09 +0800

    更新依赖

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Licensed under the Apache License, Version 2.0 (the "License"),
 # see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
 
-__author__="Zhang Yi <loeyae@gmail.com>"
-__date__ ="$2019-2-19 9:16:07$"
-
+__author__ = "Zhang Yi <loeyae@gmail.com>"
+__date__ = "$2019-2-19 9:16:07$"
 
 from setuptools import setup, find_packages
 
 setup(
-    name = "cdspider_bbs",
-    version = "0.1",
-    description = "数据采集框架论坛采集",
-    author = 'Zhang Yi',
-    author_email = 'loeyae@gmail.com',
-    license = "Apache License, Version 2.0",
+    name="cdspider_bbs",
+    version="0.1.3",
+    description="数据采集框架论坛采集",
+    author='Zhang Yi',
+    author_email='loeyae@gmail.com',
+    license="Apache License, Version 2.0",
     url="https://github.com/loeyae/lspider_bbs.git",
-    install_requires = [
-        'cdspider>=0.1',
+    install_requires=[
+        'cdspider>=0.1.4',
     ],
-    packages = find_packages(),
+    packages=find_packages(),
 
-    entry_points = {
+    entry_points={
         'cdspider.handler': [
             'bbs-list=cdspider_bbs.handler:BbsListHandler',
             'bbs-item=cdspider_bbs.handler:BbsItemHandler',
